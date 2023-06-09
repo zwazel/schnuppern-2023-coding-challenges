9ab350169ffbb10353a8dcfc49172c21b390a580
Tim Irmler <zwazel@hotmail.com>
2023-06-09 10:57:03 +0200

    first few challenges

---
+++ src/main/java/dev/zwazel/easy/WordReversal.java
@@ -0,0 +1,15 @@
+package dev.zwazel.easy;
+
+/**
+ * Write a program that takes a string of words as input and outputs the words in reverse order.
+ * For example, if the input is “the quick brown fox”, the output should be “fox brown quick the”
+ */
+public class WordReversal {
+    public static void main(String[] args) {
+        System.out.println(reverse("the quick brown fox"));
+    }
+
+    public static String reverse(String input) {
+        return input;
+    }
+}
---
+++ src/main/java/dev/zwazel/hard/Factorial.java
@@ -0,0 +1,14 @@
+package dev.zwazel.hard;
+
+/**
+ * Write a program that calculates and prints out the factorial of a given number using recursion.
+ */
+public class Factorial {
+    public static void main(String[] args) {
+
+    }
+
+    public static int factorial(int number) {
+        return 0;
+    }
+}
