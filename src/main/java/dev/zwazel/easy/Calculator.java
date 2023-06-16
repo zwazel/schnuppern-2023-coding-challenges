579795870b98ccf6605146bfbe214832cd14ae3b
Tim Irmler <zwazel@hotmail.com>
2023-06-16 13:05:10 +0200

    calculator

---
+++ src/main/java/dev/zwazel/easy/Calculator.java
@@ -0,0 +1,22 @@
+package dev.zwazel.easy;
+
+/**
+ * Warmup challenge: Write a calculator that can add, subtract, multiply, and divide two numbers.
+ */
+public class Calculator {
+    public static int add(int a, int b) {
+        return 0;
+    }
+
+    public static int subtract(int a, int b) {
+        return 0;
+    }
+
+    public static int multiply(int a, int b) {
+        return 0;
+    }
+
+    public static int divide(int a, int b) {
+        return 0;
+    }
+}
---
+++ src/test/java/dev/zwazel/easy/CalculatorTest.java
@@ -0,0 +1,28 @@
+package dev.zwazel.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void add() {
+        assertEquals(2, Calculator.add(1, 1));
+    }
+
+    @Test
+    void subtract() {
+        assertEquals(0, Calculator.subtract(1, 1));
+    }
+
+    @Test
+    void multiply() {
+        assertEquals(20, Calculator.multiply(4, 5));
+    }
+
+    @Test
+    void divide() {
+        assertEquals(2, Calculator.divide(4, 2));
+    }
+}
