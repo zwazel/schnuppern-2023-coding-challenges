106965627ff17fc170a2a547535f0f3bd42c9633
Tim Irmler <zwazel@hotmail.com>
2023-06-27 11:00:39 +0200

    cleanup

--- src/main/java/dev/zwazel/easy/Calculator.java
+++ src/main/java/dev/zwazel/easy/Calculator.java
@@ -1,15 +1,11 @@
 package dev.zwazel.easy;
 
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-
 /**
  * Warmup challenge: Write a calculator that can add, subtract, multiply, and divide two numbers.
  */
 public class Calculator {
-    public static float calculate(char operator, float number1, float number2){
-        switch (operator){
+    public static float calculate(char operator, float number1, float number2) {
+        switch (operator) {
             case '+':
                 //TODO add
                 break;
